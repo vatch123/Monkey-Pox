e98177213c50dcf1a1739b5498d923db1d1e60c0
vatch123 <vatsalyachaubey19980@gmail.com>
2022-11-26 14:27:24 -0800

    Added more visualizations

--- src/visualizations.py
+++ src/visualizations.py
@@ -5,6 +5,7 @@ import pandas as pd
 import plotly.express as px
 
 from src.utils import get_highest_cases
+from src.utils import read_data
 
 
 def total_cases_on_map(df_worldwide_cases: pd.DataFrame, df_daily_cases: pd.DataFrame):
@@ -58,3 +59,20 @@ def daily_changes(df_worldwide_cases: pd.DataFrame, df_daily_cases: pd.DataFrame
                  x="Date", y="Cases", color="Country",
                   title="Daily Changes")
     fig.show()
+
+def cases_cities(df_detection_timeline: pd.DataFrame):
+    df_cities = df_detection_timeline[['City']]
+    df_cities = df_cities.dropna()
+    df_cities = df_cities.value_counts(['City']).reset_index(name='Total Cases')
+
+    # Pick the top 10 cities with the highest case counts
+    df = df_cities.loc[:10]
+    fig = px.bar(df, x='City', y='Total Cases', title='Number of cases in city', color='City')
+    fig.show()
+
+
+if __name__=="__main__":
+    df_worldwide_cases = read_data('Monkey_Pox_Cases_Worldwide_Cleaned.csv')
+    df_daily_cases = read_data('Daily_Country_Wise_Confirmed_Cases.csv')
+    df_detection_timeline = read_data('Worldwide_Case_Detection_Timeline_Cleaned.csv')
+    cases_cities(df_detection_timeline)
