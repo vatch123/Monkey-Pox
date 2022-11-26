16561283e3817e5d062344aafd83984437e6ed11
1001shiyuan <72235555+1001shiyuan@users.noreply.github.com>
2022-11-26 11:39:22 -0800

    Update visualizations.py

--- src/visualizations.py
+++ src/visualizations.py
@@ -58,3 +58,65 @@ def daily_changes(df_worldwide_cases: pd.DataFrame, df_daily_cases: pd.DataFrame
                  x="Date", y="Cases", color="Country",
                   title="Daily Changes")
     fig.show()
+    
+import pandas as pd
+pd.set_option('display.max_columns', None)
+df = pd.read_csv("C:/0-ece143/project/data/Worldwide_Case_Detection_Timeline.csv", encoding='unicode_escape')
+import matplotlib.pyplot as plt
+
+date = df['Date_confirmation'].tolist()
+country = df['Country'].tolist()
+world_counter = {}
+us_counter = {}
+
+for i in range(len(date)):
+    dat = date[i]
+    cou = country[i]
+    if dat not in world_counter.keys():
+        world_counter[dat] = 1
+    else:
+        world_counter[dat] += 1
+    if cou == 'United States':
+        if dat not in us_counter.keys():
+            us_counter[dat] = 1
+        else:
+            us_counter[dat] += 1
+            
+world_date = list(world_counter.keys())
+world_count = list(world_counter.values())
+plt.plot(world_date, world_count, label="World")
+us_date = list(us_counter.keys())
+us_count = list(us_counter.values())
+plt.plot(us_date, us_count, label="US")
+plt.legend()
+plt.xticks(world_date[25::25])
+plt.title('Confirmed Cases in the World and the US')
+plt.xlabel('Dates')
+plt.ylabel('Number of Cases')
+plt.savefig('Timeline.png')
+
+import pandas as pd
+pd.set_option('display.max_columns', None)
+df = pd.read_csv("C:/0-ece143/project/data/Monkey_Pox_Cases_Worldwide.csv", encoding='unicode_escape')
+import matplotlib.pyplot as plt
+from statistics import mean
+
+conf = mean(df['Confirmed_Cases'].tolist())
+hosp = mean(df['Hospitalized'].tolist())
+trav = mean(df['Travel_History_Yes'].tolist())
+
+plt.bar(['US','World'], [24403, conf])
+plt.title('Confirmed')
+plt.ylabel('Number of Cases')
+plt.savefig('Confirmed Histogram.png')
+plt.clf()
+plt.bar(['US','World'], [4, hosp])
+plt.title('Hospitalized')
+plt.ylabel('Number of Cases')
+plt.savefig('Hospitalized Histogram.png')
+plt.clf()
+plt.bar(['US','World'], [41, trav])
+plt.title('Travel History')
+plt.ylabel('Number of Cases')
+plt.savefig('Travel Histogram.png')
+plt.clf()
